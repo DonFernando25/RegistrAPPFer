3b97fd008351274ab6086034861a806d0b70e7fc
DonFernando25 <fe.arevaloa@duocuc.cl>
2024-09-06 17:02:25 -0400

    first committ

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+import { NavController } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage]
+})
+
+
+export class HomePageModule {
+
+  constructor(private navCtrl: NavController) {}
+
+
+  navigateToLogin() {
+    this.navCtrl.navigateForward('/login');
+  }
+}
